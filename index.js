1bf22bcc57e8d3dfa218a658ef5f48e4ac00f523
randa yahya <randaalrayan96@gmial.com>
2020-05-15 22:08:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
-}
+//const sinon = document.getElementById('input');
+//function addingEventListener(event) {
+ 
+  //  sinon.addEventListener('click')
+   // alert('iam clicking');
+//}
+const sinon = document.getElementById('input');
+sinon.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
